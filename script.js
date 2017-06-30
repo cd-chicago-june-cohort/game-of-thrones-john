ce28b1368c2a09ab59e4bca02f9968615c0f6871
JP Doherty <jpdoherty90@gmail.com>
2017-06-29 22:58:20 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    var $img = $("img");
+
+    $img.click(function() {
+
+        var num = $(this).attr("id");
+
+        var url = "http://anapioficeandfire.com/api/houses/" + num;
+
+        var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+        //var familyName, words, titles;
+
+        $.get(proxy + url, function(info) {
+
+            var familyName = info.name;
+            var familyWords = info.words;
+            var familyTitles = info.titles;
+            
+            var str = "<legend>House Details</legend><p>Name: " + familyName + '</p><p>Words: "' + familyWords + '"</p><p>Titles: ';
+
+            for (var i = 0; i < familyTitles.length - 1; i++) {
+                str += familyTitles[i] + ", ";
+            }
+
+            str += familyTitles[familyTitles.length - 1] + ".";
+
+
+            str += "</p>";
+
+            $("fieldset").html(str);
+
+        }, "json");
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
